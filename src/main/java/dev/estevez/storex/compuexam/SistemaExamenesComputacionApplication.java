94893ed148570db0c98a60045435b7c60227a5e4
Juan Carlos Estevez Vargas <juank2001estevez@gmail.com>
2023-02-01 18:44:10 -0500

    Creación del proyecto

---
+++ src/main/java/dev/estevez/storex/compuexam/SistemaExamenesComputacionApplication.java
@@ -0,0 +1,13 @@
+package dev.estevez.storex.compuexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaExamenesComputacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaExamenesComputacionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/estevez/storex/compuexam/SistemaExamenesComputacionApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.estevez.storex.compuexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaExamenesComputacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
