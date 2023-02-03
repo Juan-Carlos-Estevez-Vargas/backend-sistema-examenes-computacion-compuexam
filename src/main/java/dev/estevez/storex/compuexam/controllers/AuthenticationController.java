156725489c5be347ada403ae116b935097230f0a
Juan Carlos Estevez Vargas <juank2001estevez@gmail.com>
2023-02-03 18:44:23 -0500

    Implementación método para obtener el usuario actual que inicia sesión

--- src/main/java/dev/estevez/storex/compuexam/controllers/AuthenticationController.java
+++ src/main/java/dev/estevez/storex/compuexam/controllers/AuthenticationController.java
@@ -1,5 +1,7 @@
 package dev.estevez.storex.compuexam.controllers;
 
+import java.security.Principal;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -7,7 +9,8 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import dev.estevez.storex.compuexam.config.JwtUtils;
 import dev.estevez.storex.compuexam.entities.JwtRequest;
 import dev.estevez.storex.compuexam.entities.JwtResponse;
+import dev.estevez.storex.compuexam.entities.Usuario;
 import dev.estevez.storex.compuexam.services.impl.UserDetailsServiceImpl;
 
 /**
@@ -24,6 +28,7 @@ import dev.estevez.storex.compuexam.services.impl.UserDetailsServiceImpl;
  *
  */
 @RestController
+@CrossOrigin("*")
 public class AuthenticationController {
 
 	@Autowired
@@ -73,6 +78,11 @@ public class AuthenticationController {
 		} catch (BadCredentialsException badCredentialsException) {
 			throw new Exception("Credenciales inválidad " + badCredentialsException.getMessage());
 		}
+	}
+	
+	@GetMapping("/actual-usuario")
+	public Usuario obtenerUsuarioActual(Principal principal) {
+		return (Usuario) this.userDetailsService.loadUserByUsername(principal.getName());
 	}
 
 }
